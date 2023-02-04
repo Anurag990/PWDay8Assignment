2d3e01739857cd8371273324b0529227a7620585
Anurag990 <114821800+Anurag990@users.noreply.github.com>
2023-02-05 01:02:09 +0530

    Add files via upload

---
+++ Day8AssQues8.java
@@ -0,0 +1,1 @@
+package PWskill;public class Day8AssQues8 {    public static void main(String [] args){        //Write a program to print numbers form 1 to 10        int i = 1;        while(i <= 10){            System.out.println(i);            i++;        }    }}
